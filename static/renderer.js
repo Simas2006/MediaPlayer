5a6852b2a78b3e871866d1a1490b9de719675e8f
BuildTools <sjuknelis@gmail.com>
2017-11-05 13:59:00 -0500

    Add renderer and default styles

--- static/renderer.js
+++ static/renderer.js
@@ -1,21 +1,29 @@
-var activePage = "main_page";
+var activePage = "MainPage";
+var params = "";
 var pageDict,page,core;
 
 class CoreAgent {
   renderPage() {
-    page = new pageDict[activePage]();
-    document.getElementById("content").innerHTML = page.static;
+    page = new pageDict[activePage](params);
+    var intervalId = setInterval(function() {
+      if ( page.static ) {
+        document.getElementById("content").innerHTML = page.static;
+        clearInterval(intervalId);
+      }
+    });
   }
-  openPage(id) {
+  openPage(id,newparams) {
     document.getElementById("content").innerHTML = "";
     activePage = id;
+    params = newparams;
     this.renderPage();
   }
 }
 
 window.onload = function() {
   pageDict = {
-   "main_page": MainPage
+    MainPage,
+    MusicAlbumPage
   };
   core = new CoreAgent();
   core.renderPage();

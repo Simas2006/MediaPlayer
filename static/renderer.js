b26fa3b41ce26303207876ee27b77b47b8ec70c2
BuildTools <sjuknelis@gmail.com>
2017-12-24 18:22:06 -0500

    Redesign + clickable sliders

--- static/renderer.js
+++ static/renderer.js
@@ -1,6 +1,7 @@
 var activePage = "MainPage";
 var params = "";
-var pageDict,page,core;
+var pageDict,page;
+var core,mcore,dcore;
 
 class CoreAgent {
   renderPage() {
@@ -23,7 +24,8 @@ class MusicCoreAgent {
     this.queue = [];
     this.firstTrigger = true;
     this.playing = false;
-    this.volume = 100;
+    this.hasSong = false;
+    this.volume = 50;
     this.audio = document.getElementById("musicaudio");
     this.audio.addEventListener("ended",this.playNextSong);
   }
@@ -34,30 +36,36 @@ class MusicCoreAgent {
       }
       return s;
     }
-    if ( mcore.queue.length < 1 ) {
+    if ( mcore.queue.length <= 0 ) {
+      mcore.hasSong = false;
+      mcore.playing = false;
       mcore.audio.currentTime = 1e6;
-      document.getElementById("musicname").innerText = "Playing: Nothing";
-      document.getElementById("controls").style.display = "none";
+      document.getElementById("musicname").innerText = "Not Playing";
+      document.getElementById("playpause").innerHTML = "&#9654;";
+      document.getElementById("timespent").innerText = "--:--";
+      document.getElementById("timeleft").innerText = "--:--";
       if ( activePage == "MusicQueuePage" ) core.openPage("MainPage","");
       return;
     }
-    document.getElementById("controls").style.display = "block";
+    var songName = decodeItAllTheWay(mcore.queue[0]);
     var source = document.getElementById("musicsrc");
-    source.src = __dirname + "/../media/music/" + decodeItAllTheWay(mcore.queue[0]);
+    source.src = __dirname + "/../media/music/" + songName;
     mcore.audio.load();
     mcore.audio.play();
-    document.getElementById("musicname").innerText = "Playing: " + decodeItAllTheWay(mcore.queue[0]);
+    document.getElementById("musicname").innerText = songName.split("/")[songName.split("/").length - 1].split(".").slice(0,-1).join(".");
     mcore.queue = mcore.queue.slice(1,mcore.audio.length);
     mcore.playing = true;
     document.getElementById("playpause").innerHTML = mcore.playing ? "||" : "&#9654;";
     if ( activePage == "MusicQueuePage" ) page.render();
   }
   addToQueue(names) {
+    this.hasSong = true;
     this.queue = this.queue.concat(names);
     if ( document.getElementById("musicaudio").ended || this.firstTrigger ) this.playNextSong();
     this.firstTrigger = false;
   }
   togglePlay() {
+    if ( ! this.hasSong ) return;
     this.playing = ! this.playing;
     document.getElementById("playpause").innerHTML = this.playing ? "||" : "&#9654;";
     if ( this.playing ) this.audio.play();
@@ -67,11 +75,60 @@ class MusicCoreAgent {
     if ( activePage == "MusicQueuePage" ) core.openPage("MainPage","");
     else core.openPage("MusicQueuePage","");
   }
-  changeVolume(dv) {
-    if ( this.volume + dv < 0 || this.volume + dv > 100 ) return;
-    this.volume += dv;
-    document.getElementById("volume").innerText = this.volume + "%";
-    this.audio.volume = this.volume / 100;
+  setTime(nt) {
+    if ( ! this.playing ) return;
+    this.audio.currentTime = nt;
+  }
+  mute() {
+    this.volume = 0;
+    dcore.drawVolumeSlider();
+  }
+}
+
+class DrawingCoreAgent {
+  constructor() {
+    this.volume = document.getElementById("volume");
+    this.volume.onclick = this.registerVolumeClick;
+    this.time = document.getElementById("time");
+    this.time.onclick = this.registerTimeClick;
+    this.drawVolumeSlider();
+    setInterval(this.drawTimeSlider,10);
+  }
+  drawVolumeSlider() {
+    var canvas = this.volume;
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#999";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "#f00";
+    var x = mcore.volume / 100 * canvas.width;
+    ctx.fillRect(x - 5,0,10,canvas.height);
+    ctx.fillStyle = "#fff";
+    ctx.font = "30px Arial";
+    ctx.fillText(mcore.volume + "%",canvas.width * .375,canvas.height * .825);
+  }
+  registerVolumeClick(event) {
+    var rect = this.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    mcore.volume = Math.round(x / this.width * 100);
+    dcore.drawVolumeSlider();
+  }
+  drawTimeSlider() {
+    var padNumbers = n => n >= 10 ? n.toString() : "0" + n;
+    var canvas = this.time;
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#999";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "#f00";
+    if ( ! mcore.hasSong ) return;
+    var x = mcore.audio.currentTime / mcore.audio.duration * canvas.width;
+    ctx.fillRect(0,0,x,canvas.height);
+    document.getElementById("timespent").innerText = padNumbers(Math.floor(mcore.audio.currentTime / 60)) + ":" + padNumbers(Math.floor((mcore.audio.currentTime % 60)));
+    document.getElementById("timeleft").innerText = "-" + padNumbers(Math.floor((mcore.audio.duration - mcore.audio.currentTime) / 60)) + ":" + padNumbers(Math.floor(((mcore.audio.duration - mcore.audio.currentTime) % 60)));
+  }
+  registerTimeClick(event) {
+    var rect = this.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    mcore.audio.currentTime = x / this.width * mcore.audio.duration;
   }
 }
 
@@ -89,6 +146,6 @@ window.onload = function() {
   };
   core = new CoreAgent();
   mcore = new MusicCoreAgent();
+  dcore = new DrawingCoreAgent();
   core.renderPage();
-  document.getElementById("controls").style.display = "none";
 }

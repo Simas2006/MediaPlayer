5746fa1621acc9e372aa64de2a8297c710752b13
Simas2006 <sjuknelis@gmail.com>
2017-12-27 15:49:34 -0500

    Add cause

--- static/login/script.js
+++ static/login/script.js
@@ -14,7 +14,7 @@ window.onload = function() {
     if ( localStorage.getItem("error").indexOf("bad decrypt") > -1 ) possibleCause = "Invalid password";
     else if ( localStorage.getItem("error").indexOf("ECONNREFUSED") > -1 ) possibleCause = "Could not connect to server";
     else if ( localStorage.getItem("error").indexOf("register token") > -1 ) possibleCause = "Server did not recognize computer/Server was reset";
-    else if ( localStorage.getItem("error").indexOf("Arbitrary server") > -1 ) possibleCause = "An error occurred with the server/Check server log for details";
+    else if ( localStorage.getItem("error").indexOf("Arbitrary server") > -1 ) possibleCause = "An error occurred with the server/Try reconnecting/Check server log for details";
     document.getElementById("error").innerText = "An error occurred:\n" + localStorage.getItem("error") + (possibleCause ? "\nPossible cause: " + possibleCause : "");
     localStorage.clear();
   }

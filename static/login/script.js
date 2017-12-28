ee828c3dca6584bff7b3d804fbe1aef88ad20dce
Simas2006 <sjuknelis@gmail.com>
2017-12-28 18:58:47 -0500

    Add language support for login page

--- static/login/script.js
+++ static/login/script.js
@@ -1,3 +1,5 @@
+var fs = require("fs");
+
 function doLogin() {
   var form = document.forms.login;
   localStorage.setItem("type",form.type.value);
@@ -9,20 +11,30 @@ function doLogin() {
 }
 
 window.onload = function() {
-  if ( localStorage.getItem("error") ) {
-    var possibleCause = "";
-    if ( localStorage.getItem("error").indexOf("bad decrypt") > -1 ) possibleCause = "Invalid password";
-    else if ( localStorage.getItem("error").indexOf("ECONNREFUSED") > -1 ) possibleCause = "Could not connect to server";
-    else if ( localStorage.getItem("error").indexOf("register token") > -1 ) possibleCause = "Server did not recognize computer/Server was reset";
-    else if ( localStorage.getItem("error").indexOf("Arbitrary server") > -1 ) possibleCause = "An error occurred with the server/Try reconnecting/Check server log for details";
-    document.getElementById("error").innerText = "An error occurred:\n" + localStorage.getItem("error") + (possibleCause ? "\nPossible cause: " + possibleCause : "");
-    localStorage.clear();
-  }
-  setInterval(function() {
-    if ( document.forms.login.type.value == "online" ) {
-      document.getElementById("creddata").style.display = "block";
-    } else {
-      document.getElementById("creddata").style.display = "none";
+  fs.readFile(__dirname + "/../lang/en_us.json",function(err,data) {
+    if ( err ) throw err;
+    var lang = JSON.parse(data.toString())["LoginPage"];
+    document.getElementById("title").innerText = lang.title;
+    document.getElementById("offline_text").innerText = lang.offline;
+    document.getElementById("online_text").innerText = lang.online;
+    document.getElementById("ip_text").innerText = lang.ip_address;
+    document.getElementById("password_text").innerText = lang.password;
+    document.getElementById("login_text").innerText = lang.login_button;
+    if ( localStorage.getItem("error") ) {
+      var possibleCause = "";
+      if ( localStorage.getItem("error").indexOf("bad decrypt") > -1 ) possibleCause = lang.possible_cause.invalid_password;
+      else if ( localStorage.getItem("error").indexOf("ECONNREFUSED") > -1 ) possibleCause = lang.possible_cause.refused_connection;
+      else if ( localStorage.getItem("error").indexOf("register token") > -1 ) possibleCause = lang.possible_cause.invalid_id;
+      else if ( localStorage.getItem("error").indexOf("Arbitrary server") > -1 ) possibleCause = lang.possible_cause.server_error;
+      document.getElementById("error").innerText = lang.error_occurred + ":\n" + localStorage.getItem("error") + (possibleCause ? "\n" + lang.cause_message + ": " + possibleCause : "");
+      localStorage.clear();
     }
-  },100);
+    setInterval(function() {
+      if ( document.forms.login.type.value == "online" ) {
+        document.getElementById("creddata").style.display = "block";
+      } else {
+        document.getElementById("creddata").style.display = "none";
+      }
+    },100);
+  });
 }

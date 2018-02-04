075322964a4a04d4082fdc6c0abd0ae121980701
Simas2006 <sjuknelis@gmail.com>
2018-02-04 17:48:38 -0500

    Use different title in /connect

--- static/connect/script.js
+++ static/connect/script.js
@@ -1,12 +1,10 @@
 var fs = require("fs");
-var remote = require("electron").remote;
 
 function doLogin() {
   var url = document.getElementById("address").value;
   if ( ! url.startsWith("http") ) url = "http://" + url;
   localStorage.setItem("stream_key",document.getElementById("password").value);
   localStorage.setItem("stream_url",url);
-  remote.getCurrentWindow().close();
 }
 
 window.onkeypress = function(event) {
@@ -20,7 +18,7 @@ window.onload = function() {
   fs.readFile(__dirname + "/../lang/" + localStorage.getItem("language"),function(err,data) {
     if ( err ) throw err;
     var lang = JSON.parse(data.toString())["LoginPage"];
-    document.getElementById("title").innerText = lang.title;
+    document.getElementById("title").innerText = lang.stream_title;
     document.getElementById("ip_text").innerText = lang.ip_address;
     document.getElementById("password_text").innerText = lang.password;
     document.getElementById("login_text").innerText = lang.login_button;

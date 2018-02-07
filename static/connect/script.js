cf21f00a021a9255e17ded96fb196877718bde9f
Simas2006 <sjuknelis@gmail.com>
2018-02-07 14:28:45 -0500

    Rewire connection box + add to start streaming

--- static/connect/script.js
+++ static/connect/script.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 
 function doLogin() {
   var url = document.getElementById("address").value;
-  if ( ! url.startsWith("http") ) url = "http://" + url;
+  if ( ! url.startsWith("http") && location.search != "?server" ) url = "http://" + url;
   localStorage.setItem("stream_key",document.getElementById("password").value);
   localStorage.setItem("stream_url",url);
 }
@@ -18,8 +18,8 @@ window.onload = function() {
   fs.readFile(__dirname + "/../lang/" + localStorage.getItem("language"),function(err,data) {
     if ( err ) throw err;
     var lang = JSON.parse(data.toString())["LoginPage"];
-    document.getElementById("title").innerText = lang.stream_title;
-    document.getElementById("ip_text").innerText = lang.ip_address;
+    document.getElementById("title").innerText = location.search == "?server" ? lang.stream_server_title : lang.stream_client_title;
+    document.getElementById("ip_text").innerText = location.search == "?server" ? lang.port : lang.ip_address;
     document.getElementById("password_text").innerText = lang.password;
     document.getElementById("login_text").innerText = lang.login_button;
     errorHandling(lang);
